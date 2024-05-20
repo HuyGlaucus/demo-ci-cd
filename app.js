054413c0a776c4c38ffa3c8bc4bc9e6c89711fe9
HuyGlaucus <nguyenhuuhuyrich@gmail.com>
2024-05-20 16:51:42 +0700

    test1222

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var cli = commandLineArgs([
 var location = cli.location;
 console.log(location);
 console.log("hello world 999");
-console.log("hello thay tien CNTT");
+console.log("hello thay hai CNTT");
 
 
 /* Fetch weather data */
